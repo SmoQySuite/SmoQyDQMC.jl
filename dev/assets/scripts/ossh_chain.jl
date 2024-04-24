2dd2ff848e788d6271c8c2a85fe127c84ee03157
Documenter.jl <documenter@juliadocs.github.io>
2024-04-24 19:09:19 +0000

    build based on f0406cc

--- dev/assets/scripts/ossh_chain.jl
+++ dev/assets/scripts/ossh_chain.jl
@@ -53,10 +53,10 @@ function run_ossh_chain_simulation(sID, Ω, α, μ, β, L, N_burnin, N_updates, 
     bin_size = div(N_updates, N_bins)
 
     # Number of fermionic time-steps in HMC update.
-    Nt = 4
+    Nt = 2
 
     # Fermionic time-step used in HMC update.
-    Δt = π/(Nt*Ω)
+    Δt = π/(2*Ω)/Nt
 
     # Initialize a dictionary to store additional information about the simulation.
     additional_info = Dict(
